63fb2cfd7700081510097a7eead66e64a24c44a0
cowardlycamper32 <g.hickman2022@gmail.com>
2025-01-17 13:10:01 +0000

    adding key checks and movement i think

--- main.lua
+++ main.lua
@@ -1,14 +1,52 @@
 ent = require "ent"
-
+direction = {
+    x = 0,
+    y = 0
+}
 function love.load()
     ent.init()
 end
 function love.draw()
     love.graphics.print("Hello World", 400, 400)
 end
+function love.update()
+    if direction.x > 1 then
+        direction.x = 1
+    elseif direction.x < -1 then
+        direction.x = -1
+    elseif direction.y > 1 then
+        direction.y = 1
+    elseif direction.y < -1 then
+        direction.y = -1
+    end
+
+end
 function love.keypressed(key)
+    ent.info(key)
     if key == "escape" then
         love.event.quit()
+    end
+    if key == "left" or key == "a" then
+        direction.x = direction.y + 1
+    elseif key == "right" or key == "d" then
+        direction.x = direction.x + -1
+    elseif key == "up" or key == "w" then
+        direction.y = direction.y + 1
+    elseif key == "down" or key == "s" then
+        direction.y = direction.y + -1
+    end
+    ent.echo(tostring(x) .. "," .. tostring(y))
+
+end
+function love.keyunpressed(key)
+    if key == "left" or key == "a" then
+        direction.x = direction.y - 1
+    elseif key == "right" or key == "d" then
+        direction.x = direction.x - -1
+    elseif key == "up" or key == "w" then
+        direction.y = direction.y - 1
+    elseif key == "down" or key == "s" then
+        direction.y = direction.y - -1
     end
 end
 function love.focus(f)
@@ -17,4 +55,9 @@ function love.focus(f)
     else
         print("FOCUSED")
     end
+end
+
+function love.quit()
+    ent.warn("quitstate Initiated")
+    ent.close()
 end
