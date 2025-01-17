5dfc5b7d695079738a4f7aaad2a8a975ee149fab
cowardlycamper32 <g.hickman2022@gmail.com>
2025-01-17 14:59:15 +0000

    one movement key works? not 2 but oposite keys negate movement

--- main.lua
+++ main.lua
@@ -3,39 +3,52 @@ direction = {
     x = 0,
     y = 0
 }
+function move(x, y, speed, dt)
+    if x == true then
+        position.x = position.x + speed * dt
+    elseif y then
+        position.y = position.y + speed * dt
+    end
+end
 function love.load()
-    ent.init()
+    position = {
+        x = 100,
+        y = 50
+    }
 end
 function love.draw()
-    love.graphics.print("Hello World", 400, 400)
+    love.graphics.rectangle("line", position.x, position.y, 200, 150)
 end
-function love.update()
-    if direction.x > 1 then
+function love.update(dt)
+    if direction.x >= 1 then
         direction.x = 1
-    elseif direction.x < -1 then
+        move(true, false, 1 * 50, dt)
+    elseif direction.x <= -1 then
         direction.x = -1
-    elseif direction.y > 1 then
+        move(true, false, -1 * 50, dt)
+    elseif direction.y >= 1 then
         direction.y = 1
-    elseif direction.y < -1 then
+        move(false, true, 1 * 50, dt)
+    elseif direction.y <= -1 then
         direction.y = -1
+        move(false, true, -1 * 50, dt)
     end
 
 end
 function love.keypressed(key)
-    ent.info(key)
     if key == "escape" then
         love.event.quit()
     end
     if key == "left" or key == "a" then
-        direction.x = direction.x + 1
-    elseif key == "right" or key == "d" then
         direction.x = direction.x + -1
+    elseif key == "right" or key == "d" then
+        direction.x = direction.x + 1
     elseif key == "up" or key == "w" then
-        direction.y = direction.y + 1
-    elseif key == "down" or key == "s" then
         direction.y = direction.y + -1
+    elseif key == "down" or key == "s" then
+        direction.y = direction.y + 1
     end
-    ent.echo(tostring(direction.x) .. "," .. tostring(direction.y))
+    print((tostring(direction.x) .. "," .. tostring(direction.y)))
 
 end
 function love.keyreleased(key)
@@ -48,7 +61,19 @@ function love.keyreleased(key)
     elseif key == "down" or key == "s" then
         direction.y = direction.y - -1
     end
-    ent.echo(tostring(direction.x) .. "," .. tostring(direction.y))
+    if not((key == "left" or key == "a") and (key == "right" or key == "d")) then
+        direction.x = 0
+    end
+
+    if not((key == "up" or key == "w") and (key == "down" or key == "s")) then
+        direction.y = 0
+    end
+    if (key == "left" or key == "a") and (key == "up" or key == "w") then
+        direction.x = -1
+        direction.y = 1
+    end
+
+    print(tostring(direction.x) .. "," .. tostring(direction.y))
 end
 function love.focus(f)
     if not f then
@@ -59,6 +84,5 @@ function love.focus(f)
 end
 
 function love.quit()
-    ent.warn("quitstate Initiated")
-    ent.close()
+    print("quitstate Initiated")
 end
